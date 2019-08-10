67eaeab790d549e0cc9335a6ea704fea7d29b281
abdurahmanfz <zandt55@gmail.com>
2019-08-11 00:53:35 +0700

    initial commit

---
+++ precache-manifest.ca6fb1ab382bb9bf7538f11c175d29db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "32c7ae29667774ebd573",
+    "url": "/js/chunk-vendors.5bcdacb2.js"
+  },
+  {
+    "revision": "35d5bbde5a128b9eddfb",
+    "url": "/js/chunk-67248944.443045f5.js"
+  },
+  {
+    "revision": "281c82cd4266e40f0a3e",
+    "url": "/js/app.dffca48f.js"
+  },
+  {
+    "revision": "7a7bf80a120af665efd216ac6bf1b1e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "074157f138de7d36db8d6be0a7c95d51",
+    "url": "/img/santolo.074157f1.jpg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "32c7ae29667774ebd573",
+    "url": "/css/chunk-vendors.4ce97d4e.css"
+  },
+  {
+    "revision": "35d5bbde5a128b9eddfb",
+    "url": "/css/chunk-67248944.92ead230.css"
+  }
+];
